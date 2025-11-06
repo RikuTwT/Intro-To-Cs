6039d83a4bc7e6ab2da3ced6cf9ace1f78d4dcdb
RikuTwT <12674@edu.wheatlandhigh.org>
2025-11-06 17:38:42 +0000

    Homework

--- Homeword.py
+++ Homeword.py
@@ -4,20 +4,22 @@
 
 # Logical Operators Practice
 
-# 9:50 - 
+# ~45 minutes
 
 
 
 '''
-
+# Your hint for the password
 hint = 'Your hint is a top lane league champ, who is the king.'
 
 
-
+# The password
 password = "Sett"
-
+ # Input to guess the password
 guess = str(input('What is your first guess at the character?: '))
 
+
+# What happens if youu get it wrong or right
 if guess == "Sett":
     print("Great guess, well done")
 if not guess == "Sett":
@@ -27,13 +29,14 @@ if not guess == "Sett":
 
 
 '''
+# Asks for your age and if you have a license
 
 age = int(input("How old are you?: "))
 
 license = str(input("Do you also have your license?: "))
 
 
-
+# If you fit the age and license requirement then it fills out
 if age >= 21 and license == 'Yes':
     print('You are old enough to rent a car :)')
 else:
@@ -43,11 +46,29 @@ else:
 
 
 
+'''
 
+# Asks your favorite champ
 favorite_champ = str(input('Who is your favorite champion in all of league of legends?: '))
 
-
+# Does something for you answer
 if favorite_champ == 'Shaco' or favorite_champ == 'LeBlanc':
     print("You are incorrect, please pick another champ :c")
 else:
     print("Thats a pretty good choice, please enjoy your game :)")
+''' 
+
+
+
+# Define your lane and your favorite league champ
+
+lane = str(input('What lane do you enjoy playing the most?: '))
+
+champ = str(input("What champ do you like playing in that lane?: "))
+
+if (lane == "Jungle" or lane == "Jg" or lane == "jg" or lane == "jungle" or lane == "Jungler" or lane == "jungler") and champ == "Viego" or champ == "Lillia":
+    print("You are awesome, I appreciate your taste in champions.")
+if (lane == "Jungle" or lane == "JG") and champ == "Shaco" or champ == "shaco" or champ == "Kayn" or champ == "kayn":
+    print("Dishonor on you and your family you skilless pumpkin eater")
+else:
+    print("You're alright IG, maybe pick a better lane/champ..")
