7a35bd259de613d9cc88b0f96c3589a3c9b97632
RikuTwT <12674@edu.wheatlandhigh.org>
2025-11-07 17:52:45 +0000

    This will take a while..

---
+++ What I think your LoL main says about you.py
@@ -0,0 +1,47 @@
+# This i what I think your LoL main says about you. 
+
+# Just a reminder this is case sensitive, so make sure to put punction and capital where they are meant to be
+
+
+Main_Champ = str(input("What is your lol main?: "))
+                 
+
+
+if Main_Champ == "Aatrox":
+    print("You're pretty cool, maybe a bit cocky sometimes, but you tend to have a good heart.")
+elif Main_Champ == "Ahri":
+    print("You're an egirl, gooner (this is targeted), or a furry. There is no other option.")
+elif Main_Champ == "Akali":
+    print("You're honestly super cool, you play a very high skill character and it shows in the combos.")
+elif Main_Champ == "Akshan":
+    print("Is your favorite movie Indiana Jones? You're cool, but you tend not to help much.")
+elif Main_Champ == 'Alistar':
+    print("This is a very interesting choice, real mains have moo cow and spam the bell. We love you guys.")
+elif Main_Champ == "Ambessa":
+    print("You like being topped, sorry but that's just the truth.")
+elif Main_Champ == "Amumu":
+    print("Ayden?! Nah seriously, you're cool, there's almost never a time you don't provide major utility to a team fight.")
+elif Main_Champ == "Anivia":
+    print("Birb. You are really funny with the walls, I love watching you troll or cancel recalls")
+elif Main_Champ == "Annie":
+    ("She's a fun champ, also I know you love just shouting TIBBERS whenever you see him. In Tibbers we trust.")
+elif Main_Champ == "Aphelios":
+    print("Is your sister also a hologram? Good Aphelios players are honestly really scary, please stay away from me with your calculating brain.")
+elif Main_Champ == "Ashe":
+    print("You're probably very good at kiting, the cross map ults are absolutely hilarious to die from.")
+elif Main_Champ == "Aurelion Sol":
+    print("Do you like space? It's quite cool, you're empowered ult is super annoying when it kills my jungle camps while I'm farming.")
+elif Main_Champ == "Aurora":
+    print("Bunny girl is so peak, I love rabbits and Aurora is a super fun champ to play.")
+elif Main_Champ == "Azir":
+    print('You are not Faker my guy, but you try your best.')
+elif Main_Champ == "Bard":
+    print("Thank you for your ult, it has saved me more time than I can count, and it makes securing towers super easy.")
+elif Main_Champ == "Bel'Veth":
+    print("You're quite strategic I'd imagine, though you never play for objectives, you're definitely a KDA player, and that isn't always bad.")
+elif Main_Champ == "Blitzcrank":
+    print("Please stay out of my games, I hate your hooks. Also do you canonically even exist anymore?")
+elif Main_Champ == "Braum":
+    print("You're a joy to play with. Jumping in front of me with your shield to save me is so hot.")
+elif Main_Champ == "Briar":
+    print("Eat eat eat, do rocks bleed? You're probably very silly, and you still love Briar even if it pisses everyone else off.")
