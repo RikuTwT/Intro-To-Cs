a4112fd0106b65bdd63d0efbad946cbea75da7fb
RikuTwT <12674@edu.wheatlandhigh.org>
2025-12-01 22:56:49 +0000

    I Will never figure this out

--- Final Project.py
+++ Final Project.py
@@ -7,4 +7,15 @@
 # IDK YET
 
 
- 
+
+    
+Question = str(input("Hello, are you here for some affirmation? (Y/N): "))
+    
+if Question == "Y" or Question == "y":
+        print("Okay, let's begin <3")
+        
+if Question == "N" or Question == "n":
+        print("That's completely okay, I hope you have a fantastic day.")
+        
+else: 
+        print("Please give an actual answer")
