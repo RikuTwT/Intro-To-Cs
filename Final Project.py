1f6abb790e8def490a4aa2a0f4ba28254f8c84e1
RikuTwT <12674@edu.wheatlandhigh.org>
2025-12-02 17:56:38 +0000

    Mrrp

--- Final Project.py
+++ Final Project.py
@@ -9,13 +9,26 @@
 
 
     
-Question = str(input("Hello, are you here for some affirmation? (Y/N): "))
+def Question():
+
+        Question = str(input("Hello, are you here for some affirmation? (Y/N): "))
     
-if Question == "Y" or Question == "y":
-        print("Okay, let's begin <3")
+        if Question == "Y" or Question == "y":
+                print("Okay, let's begin <3")
         
-if Question == "N" or Question == "n":
-        print("That's completely okay, I hope you have a fantastic day.")
+        if Question == "N" or Question == "n":
+                print("That's completely okay, I hope you have a fantastic day.")
+                
+                
+def looks_affirmation():
         
-else: 
-        print("Please give an actual answer")
+                
+                
+                
+Question()
+
+
+Looks = str(input("Would you like affirmations on your looks? (Y/N): "))
+
+if Looks == "Y" or Looks = "y":
+        looks_affirmation()
